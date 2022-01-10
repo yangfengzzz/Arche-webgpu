7b537ed9061d3b8509d8bd3f5481a0f973bff890
yangfeng <yangfengzzz@hotmail.com>
2022-01-10 19:13:32 +0800

    Initial Commit

---
+++ DigitalArche/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DigitalArche
+//
+//  Created by 杨丰 on 2022/1/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
