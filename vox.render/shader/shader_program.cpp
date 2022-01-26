36ea80febcbbb1f41e2d2c48f3f507815350551a
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-26 17:52:06 +0800

    add ShaderProgram

---
+++ vox.render/shader/shader_program.cpp
@@ -0,0 +1,31 @@
+//
+//  shader_program.cpp
+//  vox.render
+//
+//  Created by 杨丰 on 2022/1/26.
+//
+
+#include "shader_program.h"
+
+namespace vox {
+ShaderProgram::ShaderProgram(wgpu::Device& device,
+                             const std::string& vertexSource,
+                             const std::string& fragmentSource):
+_device(device) {
+    _createProgram(vertexSource, fragmentSource);
+}
+
+void ShaderProgram::_createProgram(const std::string& vertexSource,
+                                   const std::string& fragmentSource) {
+    wgpu::ShaderModuleDescriptor desc;
+    wgpu::ShaderModuleWGSLDescriptor wgslDesc;
+    desc.nextInChain = &wgslDesc;
+
+    wgslDesc.source = vertexSource.c_str();
+    _vertexShader = _device.CreateShaderModule(&desc);
+    
+    wgslDesc.source = fragmentSource.c_str();
+    _fragmentShader = _device.CreateShaderModule(&desc);
+}
+
+}
---
+++ vox.render/shader/shader_program.h
@@ -0,0 +1,39 @@
+//
+//  shader_program.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2022/1/26.
+//
+
+#ifndef shader_program_hpp
+#define shader_program_hpp
+
+#include <dawn/webgpu_cpp.h>
+#include <string>
+
+namespace vox {
+/**
+ * Shader program, corresponding to the GPU shader program.
+ */
+class ShaderProgram {
+public:
+    ShaderProgram(wgpu::Device& device,
+                  const std::string& vertexSource,
+                  const std::string& fragmentSource);
+    
+private:
+    /**
+     * init and link program with shader.
+     */
+    void _createProgram(const std::string& vertexSource,
+                        const std::string& fragmentSource);
+    
+private:
+    wgpu::ShaderModule _vertexShader;
+    wgpu::ShaderModule _fragmentShader;
+    wgpu::Device& _device;
+};
+
+}
+
+#endif /* shader_program_hpp */
