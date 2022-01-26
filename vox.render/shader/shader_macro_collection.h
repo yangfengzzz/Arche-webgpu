691f2505fe9c823eddc6826d83fc97c861889e49
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-26 20:09:13 +0800

    add ShaderMacroCollection

---
+++ vox.render/shader/shader_macro_collection.h
@@ -0,0 +1,45 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#ifndef shader_macro_collection_hpp
+#define shader_macro_collection_hpp
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <dawn/webgpu_cpp.h>
+
+namespace vox {
+/**
+ * Shader macro collection.
+ */
+struct ShaderMacroCollection {
+    static std::unordered_map<std::string, double> defaultValue;
+    
+    static std::vector<wgpu::ConstantEntry> createDefaultFunction();
+    
+    /**
+     * Union of two macro collection.
+     * @param left - input macro collection
+     * @param right - input macro collection
+     * @param result - union output macro collection
+     */
+    static void unionCollection(const ShaderMacroCollection &left, const ShaderMacroCollection &right,
+                                ShaderMacroCollection &result);
+    
+    size_t hash() const;
+    
+private:
+    friend class ShaderProgram;
+    
+    friend class ShaderData;
+    
+    std::unordered_map<std::string, double> _value{};
+};
+
+}
+
+#endif /* shader_macro_collection_hpp */
