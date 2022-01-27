f101f3aead1b523f0f140a92056380607f9bce73
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-27 13:18:29 +0800

    add buffer

---
+++ vox.render/graphics/buffer.cpp
@@ -0,0 +1,78 @@
+//
+//  buffer.cpp
+//  vox.render
+//
+//  Created by 杨丰 on 2022/1/27.
+//
+
+#include "buffer.h"
+
+namespace vox {
+Buffer::Buffer(wgpu::Device& device, uint64_t byteLength, wgpu::BufferUsage bufferUsage):
+_device(device) {
+    wgpu::BufferDescriptor desc;
+    desc.usage = bufferUsage;
+    desc.size = byteLength;
+    _nativeBuffer = device.CreateBuffer(&desc);
+}
+
+Buffer::Buffer(wgpu::Device& device, std::vector<uint8_t>& data, wgpu::BufferUsage bufferUsage) :
+_device(device) {
+    wgpu::BufferDescriptor desc;
+    desc.usage = bufferUsage;
+    desc.size = data.size();
+    _nativeBuffer = device.CreateBuffer(&desc);
+    
+    setData(data, 0, data.size());
+}
+
+void Buffer::setData(std::vector<uint8_t>& data,
+                     size_t bufferByteOffset,
+                     size_t dataLength,
+                     size_t dataOffset) {
+    struct UserData {
+        Buffer* dst_ptr;
+        uint8_t* src_ptr;
+        size_t length;
+        size_t bufferByteOffset;
+        size_t dataLength;
+        size_t dataOffset;
+    };
+    UserData user{this, data.data(), data.size(), bufferByteOffset, dataLength, dataOffset};
+    
+    _nativeBuffer.MapAsync(wgpu::MapMode::Write, 0, data.size(), [](WGPUBufferMapAsyncStatus status, void * userdata) {
+        if (static_cast<wgpu::BufferMapAsyncStatus>(status) == wgpu::BufferMapAsyncStatus::Success) {
+            UserData* user = static_cast<UserData*>(userdata);
+            memcpy(user->dst_ptr->_nativeBuffer.GetMappedRange(user->dataOffset, user->dataLength),
+                   user->src_ptr + user->bufferByteOffset, user->length);
+            user->dst_ptr->_nativeBuffer.Unmap();
+        }
+    }, &user);
+}
+
+void Buffer::getData(std::vector<uint8_t>& data,
+                     size_t bufferByteOffset,
+                     size_t dataLength,
+                     size_t dataOffset) {
+    struct UserData {
+        Buffer* dst_ptr;
+        uint8_t* src_ptr;
+        size_t length;
+        size_t bufferByteOffset;
+        size_t dataLength;
+        size_t dataOffset;
+    };
+    UserData user{this, data.data(), data.size(), bufferByteOffset, dataLength, dataOffset};
+    
+    _nativeBuffer.MapAsync(wgpu::MapMode::Write, 0, data.size(), [](WGPUBufferMapAsyncStatus status, void * userdata) {
+        if (static_cast<wgpu::BufferMapAsyncStatus>(status) == wgpu::BufferMapAsyncStatus::Success) {
+            UserData* user = static_cast<UserData*>(userdata);
+            memcpy(user->src_ptr + user->bufferByteOffset,
+                   user->dst_ptr->_nativeBuffer.GetMappedRange(user->dataOffset, user->dataLength),
+                   user->length);
+            user->dst_ptr->_nativeBuffer.Unmap();
+        }
+    }, &user);
+}
+
+}
---
+++ vox.render/graphics/buffer.h
@@ -0,0 +1,60 @@
+//
+//  buffer.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2022/1/27.
+//
+
+#ifndef buffer_hpp
+#define buffer_hpp
+
+#include <dawn/webgpu_cpp.h>
+#include <vector>
+
+namespace vox {
+class Buffer {
+public:
+    /**
+     * Create Buffer.
+     * @param device - Device
+     * @param byteLength - Byte length
+     * @param bufferUsage - Buffer usage
+     */
+    Buffer(wgpu::Device& device, uint64_t byteLength, wgpu::BufferUsage bufferUsage);
+    
+    /**
+     * Create Buffer.
+     * @param device - Device
+     * @param data - Byte
+     * @param bufferUsage - Buffer usage
+     */
+    Buffer(wgpu::Device& device, std::vector<uint8_t>& data, wgpu::BufferUsage bufferUsage);
+    
+    /**
+     * Set buffer data.
+     * @param data - Input buffer data
+     * @param bufferByteOffset - Buffer byte offset
+     * @param dataLength - Data length
+     * @param dataOffset - Buffer byte offset
+     */
+    void setData(std::vector<uint8_t>& data, size_t bufferByteOffset = 0,
+                 size_t dataLength = 0, size_t dataOffset = 0);
+    
+    /**
+     * Set buffer data.
+     * @param data - Input buffer data
+     * @param bufferByteOffset - Buffer byte offset
+     * @param dataLength - Data length
+     * @param dataOffset - Buffer byte offset
+     */
+    void getData(std::vector<uint8_t>& data, size_t bufferByteOffset = 0,
+                 size_t dataLength = 0, size_t dataOffset = 0);
+    
+private:
+    wgpu::Device& _device;
+    wgpu::Buffer _nativeBuffer;
+};
+
+}
+
+#endif /* buffer_hpp */
