eda38455a24929bab90698324d25cd28fd3234df
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-27 16:42:42 +0800

    finish unlit shader

--- vox.render/shaderlib/unlit_wgsl.cpp
+++ vox.render/shaderlib/unlit_wgsl.cpp
@@ -21,7 +21,39 @@ const std::string& UnlitVertexWGSL::compile(const ShaderMacroCollection& macros)
 }
 
 void UnlitVertexWGSL::_createShaderSource(size_t hash, const ShaderMacroCollection& macros) {
-    
+    std::string source = "\n"
+    "  [[block]]struct uPMatrixUniforms {\n"
+    "        [[size(64)]]m: mat4x4<f32>;\n"
+    "  };\n"
+    "\n"
+    "  [[block]]struct uMVMatrixUniforms {\n"
+    "    [[size(64)]]m: mat4x4<f32>;\n"
+    "  };\n"
+    "\n"
+    "  [[group(0), binding(0)]]\n"
+    "  var<uniform> uPMatrix: uPMatrixUniforms;\n"
+    "  [[group(0), binding(1)]]\n"
+    "  var<uniform> uMVMatrix: uMVMatrixUniforms;\n"
+    "\n"
+    "  struct VertexInput {\n"
+    "      [[location(0)]] aVertexPosition: vec3<f32>;\n"
+    "      [[location(1)]] aVertexColor: vec3<f32>;\n"
+    "      [[location(4)]] aVertexUV: vec2<f32>;\n"
+    "  };\n"
+    "\n"
+    "  struct Output {\n"
+    "    [[location(0)]] vColor: vec3<f32>;\n"
+    "    [[builtin(position)]] Position: vec4<f32>;\n"
+    "  };\n"
+    "\n"
+    "  [[stage(vertex)]]\n"
+    "  fn main(vertexInput: VertexInput) -> Output {\n"
+    "    var output: Output;\n"
+    "    output.Position = uPMatrix.m * uMVMatrix.m * vec4<f32>(vertexInput.aVertexPosition, 1.0);\n"
+    "    output.vColor = vertexInput.aVertexPosition;\n"
+    "    return output;\n"
+    "  } \n";
+    _cache[hash] = std::move(source);
 }
 
 
@@ -42,7 +74,12 @@ const std::string& UnlitFragmentWGSL::compile(const ShaderMacroCollection& macro
 }
 
 void UnlitFragmentWGSL::_createShaderSource(size_t hash, const ShaderMacroCollection& macros) {
-    
+    std::string source = "\n"
+    "[[stage(fragment)]]\n"
+    "fn main([[location(0)]] vColor: vec3<f32>) ->  [[location(0)]] vec4<f32> {\n"
+    "  return vec4<f32>(vColor, 1.0);\n"
+    "}\n";
+    _cache[hash] = std::move(source);
 }
 
 WGSLPtr fragmentVertex() {
