1e7982d3838fabf8e0599980cc24ddf17a8c99be
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-27 21:36:38 +0800

    add more into app

--- apps/primitive_app.cpp
+++ apps/primitive_app.cpp
@@ -13,8 +13,23 @@
 #include "controls/orbit_control.h"
 
 namespace vox {
+namespace {
+class MoveScript : public Script {
+public:
+    MoveScript(Entity* entity): Script(entity) {}
+    
+    void onUpdate(float deltaTime) override {
+        _rTri += 90 * deltaTime;
+        entity()->transform->setRotation(0, _rTri, 0);
+    }
+    
+private:
+    float _rTri = 0;
+};
+
+}
+
 void PrimitiveApp::loadScene(uint32_t width, uint32_t height) {
-//    _scene->background.solidColor = Color(0.5, 0.2, 0.6, 1.0);
     auto rootEntity = _scene->createRootEntity();
     
     auto cameraEntity = rootEntity->createChild();
@@ -25,11 +40,18 @@ void PrimitiveApp::loadScene(uint32_t width, uint32_t height) {
     cameraEntity->addComponent<control::OrbitControl>();
     
     auto cubeEntity = rootEntity->createChild();
+    cubeEntity->addComponent<MoveScript>();
     auto renderer = cubeEntity->addComponent<MeshRenderer>();
     renderer->setMesh(PrimitiveMesh::createCuboid(_device, 1));
     auto material = std::make_shared<UnlitMaterial>(_device);
-    material->setBaseColor(Color(0.6, 0.4, 0.7, 1.0));
     renderer->setMaterial(material);
+    
+    auto sphereEntity = rootEntity->createChild();
+    sphereEntity->transform->setPosition(0, 5, 0);
+    sphereEntity->addComponent<MoveScript>();
+    auto sphereRenderer = sphereEntity->addComponent<MeshRenderer>();
+    sphereRenderer->setMesh(PrimitiveMesh::createSphere(_device, 1));
+    sphereRenderer->setMaterial(material);
 }
 
 }
