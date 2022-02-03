eab7da00a84b1b245c2d36ddab288b6b248bb218
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-03 15:42:09 +0800

    add WGSLNormalVert

---
+++ vox.render/shaderlib/functors/wgsl_normal_vert.cpp
@@ -0,0 +1,29 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "wgsl_normal_vert.h"
+#include <fmt/core.h>
+
+namespace vox {
+WGSLNormalVert::WGSLNormalVert(const std::string& input, const std::string& output):
+_input(input),
+_output(output) {
+}
+
+void WGSLNormalVert::operator()(std::string& source, const ShaderMacroCollection& macros) {
+    if (macros.contains(HAS_NORMAL)) {
+        if (macros.contains(HAS_TANGENT) && macros.contains(HAS_NORMAL_TEXTURE)) {
+            source += "var normalW = normalize( Mat3x3<f32>(u_normalMat) * normal.xyz );";
+            source += "var tangentW = normalize( Mat3x3<f32>(u_normalMat) * tangent.xyz );";
+            source += "var bitangentW = cross( normalW, tangentW ) * tangent.w;";
+            source += "v_TBN = Mat3x3<f32>( tangentW, bitangentW, normalW );";
+        } else {
+            source += fmt::format("{}.v_normal = normalize(Mat3x3<f32>(u_normalMat) * normal);\n", _output);
+        }
+    }
+}
+
+}
