ee780f44c32c2134d58a94cec3d973d0228368a3
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-03 15:28:09 +0800

    add color vert

---
+++ vox.render/shaderlib/functors/wgsl_color_vert.h
@@ -0,0 +1,25 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#ifndef wgsl_color_vert_hpp
+#define wgsl_color_vert_hpp
+
+#include "wgsl_encoder.h"
+
+namespace vox {
+class WGSLColorVert {
+public:
+    WGSLColorVert(const std::string& input, const std::string& output);
+
+    void operator()(std::string& source, const ShaderMacroCollection& macros);
+    
+private:
+    const std::string _input;
+    const std::string _output;
+};
+
+}
+#endif /* wgsl_color_vert_hpp */
