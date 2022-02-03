6de01eb333b4cd34fcb85c367d41866210fb8258
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-03 15:25:11 +0800

    add WGSLBeginNormalVert

---
+++ vox.render/shaderlib/functors/wgsl_begin_normal_vert.h
@@ -0,0 +1,28 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#ifndef wgsl_begin_normal_vert_hpp
+#define wgsl_begin_normal_vert_hpp
+
+#include "wgsl_encoder.h"
+
+namespace vox {
+class WGSLBeginNormalVert {
+public:
+    WGSLBeginNormalVert(const std::string& input, const std::string& output);
+    
+    void operator()(std::string& source, const ShaderMacroCollection& macros);
+    
+private:
+    const std::string _input;
+    const std::string _output;
+    
+    std::string _formatTemplate;
+};
+
+}
+
+#endif /* wgsl_begin_normal_vert_hpp */
