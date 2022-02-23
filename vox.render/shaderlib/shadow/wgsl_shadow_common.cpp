d35b1a41bef2eb92c2f44fda4d4371cd3a99b7bf
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-23 09:53:13 +0800

    add WGSLShadowCommon

---
+++ vox.render/shaderlib/shadow/wgsl_shadow_common.cpp
@@ -0,0 +1,14 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "wgsl_shadow_common.h"
+
+namespace vox {
+void WGSLShadowCommon::operator()(WGSLEncoder& encoder, const ShaderMacroCollection& macros) {
+    
+}
+
+}
