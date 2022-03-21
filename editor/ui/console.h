c2e79077542f5a8de75367cf42646bdeb373d4a3
yangfengzzz <yangfengzzz@hotmail.com>
2022-03-21 16:49:49 +0800

    add view

---
+++ editor/ui/console.h
@@ -0,0 +1,81 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#ifndef console_hpp
+#define console_hpp
+
+#include "ui/widgets/panel_transformables/panel_window.h"
+#include "ui/widgets/layout/group.h"
+#include "ui/widgets/texts/text_colored.h"
+#include <glog/logging.h>
+
+namespace vox {
+using namespace ui;
+
+namespace editor {
+namespace ui {
+class Console : public PanelWindow {
+public:
+    /**
+     * Constructor
+     * @param p_title p_title
+     * @param p_opened p_opened
+     * @param p_windowSettings p_windowSettings
+     */
+    Console(const std::string& p_title,
+            bool p_opened,
+            const PanelWindowSettings& p_windowSettings);
+    
+    /**
+     * Method called when a log event occured
+     * @param p_logData
+     */
+//    void onLogIntercepted(const OvDebug::LogData& p_logData);
+    
+    /**
+     * Called when the scene plays. It will clear the console if the "Clear on play" settings is on
+     */
+    void clearOnPlay();
+    
+    /**
+     * Clear the console
+     */
+    void clear();
+    
+    /**
+     * Filter logs using defined filters
+     */
+    void filterLogs();
+    
+    /**
+     * Verify if a given log level is allowed by the current filter
+     * @param p_logLevel p_logLevel
+     */
+    bool isAllowedByFilter(google::LogSeverity p_logLevel);
+    
+private:
+    void setShowDefaultLogs(bool p_value);
+    void setShowInfoLogs(bool p_value);
+    void setShowWarningLogs(bool p_value);
+    void setShowErrorLogs(bool p_value);
+    
+private:
+    Group* _logGroup;
+    std::unordered_map<TextColored*, google::LogSeverity> m_logTextWidgets;
+    
+    bool _clearOnPlay = true;
+    bool _showDefaultLog = true;
+    bool _showInfoLog = true;
+    bool _showWarningLog = true;
+    bool _showErrorLog = true;
+};
+
+
+
+}
+}
+}
+#endif /* console_hpp */
---
+++ editor/ui/game_view.h
@@ -0,0 +1,59 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#ifndef game_view_hpp
+#define game_view_hpp
+
+#include "view.h"
+
+namespace vox {
+using namespace ui;
+
+namespace editor {
+namespace ui {
+class GameView : public View {
+public:
+    /**
+     * Constructor
+     * @param p_title p_title
+     * @param p_opened p_opened
+     * @param p_windowSettings p_windowSettings
+     */
+    GameView(const std::string& p_title,
+             bool p_opened,
+             const PanelWindowSettings& p_windowSettings);
+    
+    /**
+     * Update the view
+     * @param p_deltaTime p_deltaTimes
+     */
+    virtual void update(float p_deltaTime) override;
+    
+    /**
+     * Custom implementation of the render method
+     */
+    virtual void _render_Impl() override;
+    
+    /**
+     * Returns true if the game view has a camera
+     */
+    bool hasCamera() const;
+    
+    /**
+     * Returns the game view camera frustum or nothing if the game isn't playing
+     */
+//    std::optional<OvRendering::Data::Frustum> GetActiveFrustum() const;
+    
+private:
+//    OvCore::SceneSystem::SceneManager& _sceneManager;
+    bool _hasCamera = false;
+};
+
+
+}
+}
+}
+#endif /* game_view_hpp */
