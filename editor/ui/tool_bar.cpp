805812d0732e210717c6325bd08c8c0e4e58affa
yangfengzzz <yangfengzzz@hotmail.com>
2022-03-23 17:36:06 +0800

    add tool bar

--- editor/ui/tool_bar.cpp
+++ editor/ui/tool_bar.cpp
@@ -5,10 +5,94 @@
 //  property of any third parties.
 
 #include "tool_bar.h"
+#include "ui/widgets/layout/spacing.h"
+#include "editor_actions.h"
+#include "editor_resources.h"
 
 namespace vox {
 namespace editor {
 namespace ui {
+
+Toolbar::Toolbar(const std::string &p_title, bool p_opened,
+                 const PanelWindowSettings &p_windowSettings,
+                 EditorResources *resource) :
+PanelWindow(p_title, p_opened, p_windowSettings),
+_resource(resource) {
+    std::string iconFolder = ":Textures/Icons/";
+    
+    _playButton = &createWidget<ButtonImage>(_resource->getTexture("Button_Play"), Vector2F{20, 20});
+    _pauseButton = &createWidget<ButtonImage>(_resource->getTexture("Button_Pause"), Vector2F{20, 20});
+    _stopButton = &createWidget<ButtonImage>(_resource->getTexture("Button_Stop"), Vector2F{20, 20});
+    _nextButton = &createWidget<ButtonImage>(_resource->getTexture("Button_Next"), Vector2F{20, 20});
+    
+    createWidget<Spacing>(0).lineBreak = false;
+    auto &refreshButton = createWidget<ButtonImage>(_resource->getTexture("Button_Refresh"), Vector2F{20, 20});
+    
+    _playButton->lineBreak = false;
+    _pauseButton->lineBreak = false;
+    _stopButton->lineBreak = false;
+    _nextButton->lineBreak = false;
+    refreshButton.lineBreak = false;
+    
+    _playButton->clickedEvent +=
+    std::bind(&EditorActions::startPlaying, EditorActions::getSingletonPtr());
+    
+    _pauseButton->clickedEvent +=
+    std::bind(&EditorActions::pauseGame, EditorActions::getSingletonPtr());
+    
+    _stopButton->clickedEvent +=
+    std::bind(&EditorActions::stopPlaying, EditorActions::getSingletonPtr());
+    
+    _nextButton->clickedEvent +=
+    std::bind(&EditorActions::nextFrame, EditorActions::getSingletonPtr());
+    
+    refreshButton.clickedEvent +=
+    std::bind(&EditorActions::refreshScripts, EditorActions::getSingletonPtr());
+    
+    EditorActions::getSingleton().editorModeChangedEvent += [this](EditorActions::EditorMode p_newMode) {
+        auto enable = [](ButtonImage *p_button, bool p_enable) {
+            p_button->disabled = !p_enable;
+            p_button->tint = p_enable ? Color{1.0f, 1.0f, 1.0f, 1.0f} : Color{1.0f, 1.0f, 1.0f, 0.15f};
+        };
+        
+        switch (p_newMode) {
+            case EditorActions::EditorMode::EDIT:
+                enable(_playButton, true);
+                enable(_pauseButton, false);
+                enable(_stopButton, false);
+                enable(_nextButton, false);
+                break;
+            case EditorActions::EditorMode::PLAY:
+                enable(_playButton, false);
+                enable(_pauseButton, true);
+                enable(_stopButton, true);
+                enable(_nextButton, true);
+                break;
+            case EditorActions::EditorMode::PAUSE:
+                enable(_playButton, true);
+                enable(_pauseButton, false);
+                enable(_stopButton, true);
+                enable(_nextButton, true);
+                break;
+            case EditorActions::EditorMode::FRAME_BY_FRAME:
+                enable(_playButton, true);
+                enable(_pauseButton, false);
+                enable(_stopButton, true);
+                enable(_nextButton, true);
+                break;
+        }
+    };
+    
+    EditorActions::getSingleton().setEditorMode(EditorActions::EditorMode::EDIT);
+}
+
+void Toolbar::_draw_Impl() {
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(10, 10));
+    
+    PanelWindow::_draw_Impl();
+    
+    ImGui::PopStyleVar();
+}
 
 
 }
--- editor/ui/tool_bar.h
+++ editor/ui/tool_bar.h
@@ -14,6 +14,8 @@ namespace vox {
 using namespace ui;
 
 namespace editor {
+class EditorResources;
+
 namespace ui {
 class Toolbar : public PanelWindow {
 public:
@@ -23,9 +25,9 @@ public:
      * @param p_opened p_opened
      * @param p_windowSettings p_windowSettings
      */
-    Toolbar(const std::string &p_title,
-            bool p_opened,
-            const PanelWindowSettings &p_windowSettings);
+    Toolbar(const std::string &p_title, bool p_opened,
+            const PanelWindowSettings &p_windowSettings,
+            EditorResources *resource);
     
     /**
      * Custom implementation of the draw method
@@ -33,10 +35,11 @@ public:
     void _draw_Impl() override;
     
 private:
-    ButtonImage *m_playButton;
-    ButtonImage *m_pauseButton;
-    ButtonImage *m_stopButton;
-    ButtonImage *m_nextButton;
+    EditorResources *_resource{nullptr};
+    ButtonImage *_playButton{nullptr};
+    ButtonImage *_pauseButton{nullptr};
+    ButtonImage *_stopButton{nullptr};
+    ButtonImage *_nextButton{nullptr};
 };
 
 }
