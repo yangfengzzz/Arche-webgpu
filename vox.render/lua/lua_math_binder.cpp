75f1282a14a9cc2574b5d2e4cdbcc4edb862fdbf
yangfengzzz <yangfengzzz@hotmail.com>
2022-03-24 08:07:15 +0800

    add sol2 binding

---
+++ vox.render/lua/lua_math_binder.cpp
@@ -0,0 +1,14 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "lua_math_binder.h"
+
+namespace vox {
+void LuaMathsBinder::bindMaths(sol::state& luaState) {
+    
+}
+
+}
---
+++ vox.render/lua/lua_math_binder.h
@@ -0,0 +1,26 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#ifndef lua_math_binder_hpp
+#define lua_math_binder_hpp
+
+#include <sol/sol.hpp>
+
+namespace vox {
+/**
+ * Handle lua maths binding
+ */
+class LuaMathsBinder {
+public:
+    /**
+     * Bind engine maths-related features to lua
+     */
+    static void bindMaths(sol::state& p_luaState);
+};
+
+
+}
+#endif /* lua_math_binder_hpp */
