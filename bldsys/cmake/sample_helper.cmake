92e5e177574caa81609f3315c61f26e1b07fae8c
yangfengzzz <yangfengzzz@hotmail.com>
2022-09-03 15:26:36 +0800

    feat: clean code

---
+++ bldsys/cmake/sample_helper.cmake
@@ -0,0 +1,175 @@
+#[[
+ Copyright (c) 2019-2021, Arm Limited and Contributors
+
+ SPDX-License-Identifier: Apache-2.0
+
+ Licensed under the Apache License, Version 2.0 the "License";
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ ]]
+
+set(SCRIPT_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+function(add_sample)
+    set(options)  
+    set(oneValueArgs ID CATEGORY AUTHOR NAME DESCRIPTION)
+    set(multiValueArgs FILES LIBS SHADER_FILES_GLSL)
+
+    cmake_parse_arguments(TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})    
+
+    add_sample_with_tags(
+        TYPE "Sample"
+        ID ${TARGET_ID}
+        CATEGORY ${TARGET_CATEGORY}
+        AUTHOR ${TARGET_AUTHOR}
+        NAME ${TARGET_NAME}
+        DESCRIPTION ${TARGET_DESCRIPTION}
+        TAGS 
+            "any"
+        FILES
+            ${SRC_FILES}
+        LIBS
+            ${TARGET_LIBS}
+        SHADER_FILES_GLSL
+            ${TARGET_SHADER_FILES_GLSL})
+endfunction()
+
+function(add_sample_with_tags)
+    set(options)
+    set(oneValueArgs ID CATEGORY AUTHOR NAME DESCRIPTION)
+    set(multiValueArgs TAGS FILES LIBS SHADER_FILES_GLSL)
+
+    cmake_parse_arguments(TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    list(APPEND TARGET_TAGS "any")
+
+    set(SRC_FILES
+        ${TARGET_ID}.h
+        ${TARGET_ID}.cpp
+    )
+
+    # Append extra files if present
+    if (TARGET_FILES)
+        list(APPEND SRC_FILES ${TARGET_FILES})
+    endif()
+
+    # Add GLSL shader files for this sample
+    if (TARGET_SHADER_FILES_GLSL)    
+        list(APPEND SHADER_FILES_GLSL ${TARGET_SHADER_FILES_GLSL})
+        foreach(SHADER_FILE_GLSL ${SHADER_FILES_GLSL})
+            list(APPEND SHADERS_GLSL "${PROJECT_SOURCE_DIR}/shaders/${SHADER_FILE_GLSL}")
+        endforeach()        
+    endif()
+
+    add_project(
+        TYPE "Sample"
+        ID ${TARGET_ID}
+        CATEGORY ${TARGET_CATEGORY}
+        AUTHOR ${TARGET_AUTHOR}
+        NAME ${TARGET_NAME}
+        DESCRIPTION ${TARGET_DESCRIPTION}
+        TAGS 
+            ${TARGET_TAGS}
+        FILES
+            ${SRC_FILES}
+        LIBS
+            ${TARGET_LIBS}
+        SHADERS_GLSL
+            ${SHADERS_GLSL})
+
+endfunction()
+
+function(vkb_add_test)
+    set(options)
+    set(oneValueArgs ID)
+    set(multiValueArgs)
+
+    cmake_parse_arguments(TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    add_project(
+        TYPE "Test"
+        ID ${TARGET_ID}
+        CATEGORY "Tests"
+        AUTHOR " "
+        NAME ${TARGET_ID}
+        DESCRIPTION " "
+        VENDOR_TAG " "
+        LIBS test_framework
+        FILES
+            ${CMAKE_CURRENT_SOURCE_DIR}/${TARGET_ID}.h
+            ${CMAKE_CURRENT_SOURCE_DIR}/${TARGET_ID}.cpp)
+endfunction()
+
+function(add_project)
+    set(options)  
+    set(oneValueArgs TYPE ID CATEGORY AUTHOR NAME DESCRIPTION)
+    set(multiValueArgs TAGS FILES LIBS SHADERS_GLSL)
+
+    cmake_parse_arguments(TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if(${TARGET_TYPE} STREQUAL "Sample")
+        set("VKB_${TARGET_ID}" ON CACHE BOOL "Build sample ${TARGET_ID}")
+    endif()
+
+    if(NOT ${VKB_${TARGET_ID}})
+        message(STATUS "${TARGET_TYPE} `${TARGET_ID}` - DISABLED")
+        return()
+    endif()
+
+    message(STATUS "${TARGET_TYPE} `${TARGET_ID}` - BUILD")
+
+    # create project (object target - reused by app target)
+    project(${TARGET_ID} LANGUAGES C CXX)
+
+    source_group("\\" FILES ${TARGET_FILES})
+
+    # Add shaders to project group
+    if (SHADERS_GLSL)
+        source_group("\\Shaders" FILES ${SHADERS_GLSL})
+    endif()
+
+    add_library(${PROJECT_NAME} STATIC ${TARGET_FILES} ${SHADERS_GLSL})
+    
+    # inherit compile definitions from framework target
+    target_compile_definitions(${PROJECT_NAME} PUBLIC $<TARGET_PROPERTY:framework,COMPILE_DEFINITIONS>)
+
+    # # inherit include directories from framework target
+    target_include_directories(${PROJECT_NAME} PUBLIC $<TARGET_PROPERTY:framework,INCLUDE_DIRECTORIES> $<TARGET_PROPERTY:plugins,INCLUDE_DIRECTORIES> ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+    target_link_libraries(${PROJECT_NAME} PRIVATE framework)
+
+    # Link against extra project specific libraries
+    if(TARGET_LIBS)
+        target_link_libraries(${PROJECT_NAME} PUBLIC ${TARGET_LIBS})
+    endif()
+
+    # capitalise the first letter of the category  (performance -> Performance) 
+    string(SUBSTRING ${TARGET_CATEGORY} 0 1 FIRST_LETTER)
+    string(TOUPPER ${FIRST_LETTER} FIRST_LETTER)
+    string(REGEX REPLACE "^.(.*)" "${FIRST_LETTER}\\1" CATEGORY "${TARGET_CATEGORY}")
+
+    if(${TARGET_TYPE} STREQUAL "Sample")
+        # set sample properties
+        set_target_properties(${PROJECT_NAME}
+            PROPERTIES 
+                SAMPLE_CATEGORY ${TARGET_CATEGORY}
+                SAMPLE_AUTHOR ${TARGET_AUTHOR}
+                SAMPLE_NAME ${TARGET_NAME}
+                SAMPLE_DESCRIPTION ${TARGET_DESCRIPTION}
+                SAMPLE_TAGS "${TARGET_TAGS}")
+
+        # add sample project to a folder
+        set_property(TARGET ${PROJECT_NAME} PROPERTY FOLDER "Samples//${CATEGORY}")
+    elseif(${TARGET_TYPE} STREQUAL "Test")
+        # add test project to a folder
+        set_property(TARGET ${PROJECT_NAME} PROPERTY FOLDER "Tests")
+    endif()
+endfunction()
---
+++ bldsys/cmake/utils.cmake
@@ -0,0 +1,42 @@
+#[[
+ Copyright (c) 2019-2021, Arm Limited and Contributors
+
+ SPDX-License-Identifier: Apache-2.0
+
+ Licensed under the Apache License, Version 2.0 the "License";
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ ]]
+
+function(scan_dirs)
+    set(options)
+    set(oneValueArgs LIST DIR)
+    set(multiValueArgs)
+
+    cmake_parse_arguments(TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if(NOT EXISTS ${TARGET_DIR})
+        message(FATAL_ERROR "Directory not found `${TARGET_DIR}`")
+    endif()
+
+    file(GLOB DIR_FILES RELATIVE ${TARGET_DIR} ${TARGET_DIR}/*)
+
+    set(DIR_LIST)
+
+    foreach(FILE_NAME ${DIR_FILES})
+        if(IS_DIRECTORY ${TARGET_DIR}/${FILE_NAME})
+            list(APPEND DIR_LIST ${FILE_NAME})
+        endif()
+    endforeach()
+
+    set(${TARGET_LIST} ${DIR_LIST} PARENT_SCOPE)
+endfunction()
