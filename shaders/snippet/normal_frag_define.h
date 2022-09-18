2ac1f62efafcc49b3bac339b526245d1238448b0
yangfeng <yangfengzzz@hotmail.com>
2022-09-18 14:11:39 +0800

    fix nrmal bug

--- shaders/snippet/normal_frag_define.h
+++ shaders/snippet/normal_frag_define.h
@@ -9,14 +9,14 @@ vec3 getNormal(){
         vec3 normal = vec3(0, 0, 1);
     #endif
 
-    normal *= float( gl_FrontFacing ) * 2.0 - 1.0;
+    // normal *= float( gl_FrontFacing ) * 2.0 - 1.0;
     return normal;
 }
 
 vec3 getNormalByNormalTexture(mat3 tbn, sampler2D normalTexture, float normalIntensity, vec2 uv){
     vec3 normal = texture(normalTexture, uv).rgb;
     normal = normalize(tbn * ((2.0 * normal - 1.0) * vec3(normalIntensity, normalIntensity, 1.0)));
-    normal *= float( gl_FrontFacing ) * 2.0 - 1.0;
+    // normal *= float( gl_FrontFacing ) * 2.0 - 1.0;
 
     return normal;
 }
