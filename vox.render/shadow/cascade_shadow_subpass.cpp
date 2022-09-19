1787a2c5618543caccc605879e9ae4cba10627a0
yangfeng <yangfengzzz@hotmail.com>
2022-09-19 15:39:54 +0800

    refactor shadow

--- vox.render/shadow/cascade_shadow_subpass.cpp
+++ vox.render/shadow/cascade_shadow_subpass.cpp
@@ -8,12 +8,14 @@
 
 #include "vox.render/camera.h"
 #include "vox.render/components_manager.h"
+#include "vox.render/entity.h"
 #include "vox.render/lighting/direct_light.h"
+#include "vox.render/lighting/light_manager.h"
 #include "vox.render/material/material.h"
 #include "vox.render/mesh/mesh.h"
 #include "vox.render/renderer.h"
 #include "vox.render/rendering/render_pass.h"
-#include "vox.render/shadow/shadow_manager.h"
+#include "vox.render/shadow/shadow_utils.h"
 
 namespace vox {
 const std::string CascadedShadowSubpass::_lightViewProjMatProperty = "u_lightViewProjMat";
@@ -51,7 +53,62 @@ void CascadedShadowSubpass::_drawElement(wgpu::RenderPassEncoder& passEncoder, c
     }
 }
 
-void CascadedShadowSubpass::_renderDirectShadowMap() {}
+void CascadedShadowSubpass::_renderDirectShadowMap() {
+    const auto& lights = LightManager::GetSingleton().directLights();
+    const uint32_t sunLightIndex = LightManager::GetSingleton().getSunLightIndex();
+    const auto& boundSphere = _shadowSliceData.splitBoundSphere;
+
+    _getCascadesSplitDistance();
+    if (sunLightIndex != -1) {
+        const auto& light = lights[sunLightIndex];
+        if (light->enableShadow) {
+            _shadowInfos.x = light->shadowStrength;
+            _shadowInfos.y = _shadowTileResolution;
+            _shadowInfos.z = float(sunLightIndex);
+
+            // prepare light and camera direction
+            Matrix4x4F lightWorld = light->entity()->transform->worldRotationQuaternion().matrix4();
+
+            _lightSide.set(lightWorld[0], lightWorld[1], lightWorld[2]);
+            _lightUp.set(lightWorld[4], lightWorld[5], lightWorld[6]);
+            _lightForward.set(-lightWorld[8], -lightWorld[9], -lightWorld[10]);
+            auto cameraWorldForward = _camera->entity()->transform->worldForward();
+
+            for (uint32_t j = 0; j < shadowCascades; j++) {
+                ShadowUtils::getBoundSphereByFrustum(_cascadesSplitDistance[j], _cascadesSplitDistance[j + 1], _camera,
+                                                     cameraWorldForward.normalized(), _shadowSliceData);
+                ShadowUtils::getDirectionLightShadowCullPlanes(_camera->frustum(), _cascadesSplitDistance[j],
+                                                               _camera->nearClipPlane(), _lightForward,
+                                                               _shadowSliceData);
+                ShadowUtils::getDirectionalLightMatrices(_lightUp, _lightSide, _lightForward, light->shadowNearPlane,
+                                                         _shadowTileResolution, _shadowSliceData);
+                _updateSingleShadowCasterShaderData(light, _shadowSliceData);
+
+                // upload pre-cascade infos.
+                const auto& center = boundSphere.center;
+                const auto& radius = boundSphere.radius;
+                const uint32_t offset = j * 4;
+                _splitBoundSpheres[offset] = center.x;
+                _splitBoundSpheres[offset + 1] = center.y;
+                _splitBoundSpheres[offset + 2] = center.z;
+                _splitBoundSpheres[offset + 3] = radius * radius;
+                _vpMatrix[j] = _shadowSliceData.viewProjectMatrix;
+
+                opaqueQueue.clear();
+                alphaTestQueue.clear();
+                transparentQueue.clear();
+                const auto& renderers = ComponentsManager::GetSingleton()._renderers;
+                for (size_t k = renderers.size() - 1; k >= 0; --k) {
+                    ShadowUtils::shadowCullFrustum(renderers[k], _shadowSliceData, opaqueQueue, alphaTestQueue,
+                                                   transparentQueue);
+                }
+                std::sort(opaqueQueue.begin(), opaqueQueue.end(), _compareFromNearToFar);
+                std::sort(alphaTestQueue.begin(), alphaTestQueue.end(), _compareFromNearToFar);
+            }
+            _existShadowMap = true;
+        }
+    }
+}
 
 void CascadedShadowSubpass::_updateReceiversShaderData() {
     for (uint32_t i = shadowCascades * 4, n = 4 * 4; i < n; i++) {
@@ -106,7 +163,52 @@ float CascadedShadowSubpass::_getFarWithRadius(float radius, float denominator) 
     return std::sqrt((radius * radius) / denominator);
 }
 
-void CascadedShadowSubpass::_updateShadowSettings() {}
+void CascadedShadowSubpass::_updateShadowSettings() {
+    auto& sceneShaderData = _scene->shaderData;
+    const wgpu::TextureFormat shadowFormat = ShadowUtils::shadowDepthFormat(shadowResolution);
+    const float resolution = ShadowUtils::shadowResolution(shadowResolution);
+    if (shadowCascades != _shadowCascadeMode) {
+        sceneShaderData.addDefine("CASCADED_COUNT" + std::to_string(shadowCascades));
+    }
+    if (shadowMode != _shadowMode) {
+        sceneShaderData.addDefine("SHADOW_MODE" + std::to_string(shadowMode));
+        _shadowMode = shadowMode;
+    }
+
+    if (shadowFormat != _shadowMapFormat || resolution != _shadowMapResolution ||
+        shadowCascades != _shadowCascadeMode) {
+        _shadowMapFormat = shadowFormat;
+        _shadowMapResolution = resolution;
+        _shadowCascadeMode = shadowCascades;
+
+        if (shadowCascades == ShadowCascadesMode::NoCascades) {
+            _shadowTileResolution = shadowResolution;
+            _shadowMapSize.set(shadowResolution, shadowResolution);
+        } else {
+            const float shadowTileResolution =
+                    ShadowUtils::getMaxTileResolutionInAtlas(shadowResolution, shadowResolution, shadowCascades);
+            _shadowTileResolution = shadowTileResolution;
+            _shadowMapSize.set(shadowTileResolution * 2, shadowCascades == ShadowCascadesMode::TwoCascades
+                                                                 ? shadowTileResolution
+                                                                 : shadowTileResolution * 2);
+        }
+
+        switch (shadowCascades) {
+            case ShadowCascadesMode::NoCascades:
+                _viewportOffsets[0].set(0, 0);
+                break;
+            case ShadowCascadesMode::TwoCascades:
+                _viewportOffsets[0].set(0, 0);
+                _viewportOffsets[1].set(_shadowTileResolution, 0);
+                break;
+            case ShadowCascadesMode::FourCascades:
+                _viewportOffsets[0].set(0, 0);
+                _viewportOffsets[1].set(_shadowTileResolution, 0);
+                _viewportOffsets[2].set(0, _shadowTileResolution);
+                _viewportOffsets[3].set(_shadowTileResolution, _shadowTileResolution);
+        }
+    }
+}
 
 void CascadedShadowSubpass::_updateSingleShadowCasterShaderData(DirectLight* light,
                                                                 const ShadowSliceData& shadowSliceData) {
