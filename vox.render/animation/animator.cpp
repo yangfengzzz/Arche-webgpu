3b498117cbcfdc913193b457553053d3fcf66e09
yangfeng <yangfengzzz@hotmail.com>
2022-09-29 14:34:56 +0800

    add animation

---
+++ vox.render/animation/animator.cpp
@@ -0,0 +1,9 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "vox.render/animation/animator.h"
+
+namespace vox {}
---
+++ vox.render/animation/animator_controller.h
@@ -0,0 +1,13 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#pragma once
+
+namespace vox {
+class AnimatorController {
+public:
+};
+}  // namespace vox
