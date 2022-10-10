55cad6f4d5a0668296bb070b3aac382774adc038
yangfeng <yangfengzzz@hotmail.com>
2022-10-10 16:15:10 +0800

    add navigation target

---
+++ navigation/main.cpp
@@ -0,0 +1,45 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "apps/primitive_app.h"
+#include "vox.render/platform/platform.h"
+
+// MARK: - Entry
+#if defined(VK_USE_PLATFORM_ANDROID_KHR)
+#include "vox.render/platform/android/android_platform.h"
+void android_main(android_app *state) {
+    vox::AndroidPlatform platform{state};
+#elif defined(VK_USE_PLATFORM_WIN32_KHR)
+#include "vox.render/platform/windows/windows_platform.h"
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+    vox::WindowsPlatform platform{hInstance, hPrevInstance, lpCmdLine, nCmdShow};
+#elif defined(VK_USE_PLATFORM_DISPLAY_KHR)
+#include "vox.render/platform/unix/unix_d2d_platform.h"
+int main(int argc, char *argv[]) {
+    vox::UnixD2DPlatform platform{argc, argv};
+#else
+
+#include "vox.render/platform/unix/unix_platform.h"
+
+int main(int argc, char* argv[]) {
+#if defined(VK_USE_PLATFORM_METAL_EXT)
+    vox::UnixPlatform platform{vox::UnixType::MAC, argc, argv};
+#elif defined(VK_USE_PLATFORM_XCB_KHR) || defined(VK_USE_PLATFORM_XLIB_KHR) || defined(VK_USE_PLATFORM_WAYLAND_KHR)
+    vox::UnixPlatform platform{vox::UnixType::Linux, argc, argv};
+#endif
+#endif
+
+    auto code = platform.Initialize({});
+    if (code == vox::ExitCode::SUCCESS) {
+        platform.SetApp(std::make_unique<vox::PrimitiveApp>());
+        code = platform.MainLoop();
+    }
+    platform.Terminate(code);
+
+#ifndef VK_USE_PLATFORM_ANDROID_KHR
+    return EXIT_SUCCESS;
+#endif
+}
---
+++ navigation/primitive_app.cpp
@@ -0,0 +1,71 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "apps/primitive_app.h"
+
+#include "vox.render/camera.h"
+#include "vox.render/entity.h"
+#include "vox.render/material/blinn_phong_material.h"
+#include "vox.render/material/unlit_material.h"
+#include "vox.render/mesh/mesh_renderer.h"
+#include "vox.render/mesh/primitive_mesh.h"
+#include "vox.toolkit/controls/orbit_control.h"
+
+namespace vox {
+namespace {
+class MoveScript : public Script {
+public:
+    explicit MoveScript(Entity* entity) : Script(entity) {}
+
+    void onUpdate(float deltaTime) override {
+        _rTri += 90 * deltaTime;
+        entity()->transform->setRotation(0, _rTri, 0);
+    }
+
+private:
+    float _rTri = 0;
+};
+
+}  // namespace
+
+void PrimitiveApp::loadScene() {
+    auto scene = _sceneManager->currentScene();
+    scene->ambientLight()->setDiffuseSolidColor(Color(1, 1, 1));
+    auto rootEntity = scene->createRootEntity();
+
+    auto cameraEntity = rootEntity->createChild();
+    cameraEntity->transform->setPosition(10, 10, 10);
+    cameraEntity->transform->lookAt(Point3F(0, 0, 0));
+    _mainCamera = cameraEntity->addComponent<Camera>();
+    cameraEntity->addComponent<control::OrbitControl>();
+
+    // init point light
+    auto light = rootEntity->createChild("light");
+    light->transform->setPosition(0, 3, 0);
+    auto pointLight = light->addComponent<PointLight>();
+    pointLight->intensity = 0.3;
+
+    auto cubeEntity = rootEntity->createChild();
+    cubeEntity->addComponent<MoveScript>();
+    auto renderer = cubeEntity->addComponent<MeshRenderer>();
+    renderer->setMesh(PrimitiveMesh::createCuboid(_device, 1));
+    auto material = std::make_shared<BlinnPhongMaterial>(_device);
+    material->setBaseColor(Color(0.4, 0.6, 0.6));
+    renderer->setMaterial(material);
+
+    auto planeEntity = rootEntity->createChild();
+    planeEntity->transform->setPosition(0, 5, 0);
+    auto planeRenderer = planeEntity->addComponent<MeshRenderer>();
+    planeRenderer->setMesh(PrimitiveMesh::createSphere(_device, 1));
+    auto texturedMaterial = std::make_shared<UnlitMaterial>(_device);
+    planeRenderer->setMaterial(texturedMaterial);
+
+    texturedMaterial->setBaseTexture(ImageManager::GetSingleton().loadTexture("Textures/wood.png"));
+
+    scene->play();
+}
+
+}  // namespace vox
