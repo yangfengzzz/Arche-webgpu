bd5400cb64caf4c1047a84079fc6db23277542e8
yangfeng <yangfengzzz@hotmail.com>
2022-10-11 13:31:24 +0800

    add converter

--- converter/fbx.cpp
+++ converter/fbx.cpp
@@ -4,23 +4,19 @@
 //  personal capacity and am not conveying any rights to any intellectual
 //  property of any third parties.
 
-#include "vox.animation/offline/fbx/fbx.h"
+#include "converter/fbx.h"
 
 #include "vox.base/logging.h"
 #include "vox.base/memory/allocator.h"
-#include "vox.math/transform.h"
 
-namespace vox {
-namespace animation {
-namespace offline {
-namespace fbx {
+namespace vox::animation::offline::fbx {
 
 FbxManagerInstance::FbxManagerInstance() : fbx_manager_(nullptr) {
     // Instantiate Fbx manager, mostly a memory manager.
     fbx_manager_ = FbxManager::Create();
 
     // Logs SDK version.
-    vox::log::Log() << "FBX importer version " << fbx_manager_->GetVersion() << "." << std::endl;
+    LOGI("FBX importer version {}.", fbx_manager_->GetVersion())
 }
 
 FbxManagerInstance::~FbxManagerInstance() {
@@ -92,14 +88,14 @@ void FbxSceneLoader::ImportScene(FbxImporter* _importer,
     if (!_initialized)  // Problem with the file to be imported.
     {
         const FbxString error = _importer->GetStatus().GetErrorString();
-        vox::log::Err() << "FbxImporter initialization failed with error: " << error.Buffer() << std::endl;
+        LOGE("FbxImporter initialization failed with error: {}", error.Buffer())
 
         if (_importer->GetStatus().GetCode() == FbxStatus::eInvalidFileVersion) {
-            vox::log::Err() << "FBX file version is " << major << "." << minor << "." << revision << "." << std::endl;
+            LOGE("FBX file version is {}.{}.{}", major, minor, revision)
         }
     } else {
         if (_importer->IsFBX()) {
-            vox::log::Log() << "FBX file version is " << major << "." << minor << "." << revision << "." << std::endl;
+            LOGE("FBX file version is {}.{}.{}", major, minor, revision)
         }
 
         // Load the scene.
@@ -115,7 +111,7 @@ void FbxSceneLoader::ImportScene(FbxImporter* _importer,
             imported = _importer->Import(scene_);
 
             if (!imported && _importer->GetStatus().GetCode() == FbxStatus::ePasswordError) {
-                vox::log::Err() << "Incorrect password." << std::endl;
+                LOGE("Incorrect password.")
 
                 // scene_ will be destroyed because imported is false.
             }
@@ -160,35 +156,35 @@ vox::simd_math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem& _system) {
     const FbxAxisSystem::EUpVector eup = _system.GetUpVector(sign);
     switch (eup) {
         case FbxAxisSystem::eXAxis: {
-            up = math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
+            up = simd_math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
             // If the up axis is X, the remain two axes will be Y And Z, so the
             // ParityEven is Y, and the ParityOdd is Z
             if (_system.GetFrontVector(sign) == FbxAxisSystem::eParityEven) {
-                at = math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
+                at = simd_math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
             } else {
-                at = math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
+                at = simd_math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
             }
             break;
         }
         case FbxAxisSystem::eYAxis: {
-            up = math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
+            up = simd_math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
             // If the up axis is Y, the remain two axes will X And Z, so the
             // ParityEven is X, and the ParityOdd is Z
             if (_system.GetFrontVector(sign) == FbxAxisSystem::eParityEven) {
-                at = math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
+                at = simd_math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
             } else {
-                at = math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
+                at = simd_math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
             }
             break;
         }
         case FbxAxisSystem::eZAxis: {
-            up = math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
+            up = simd_math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
             // If the up axis is Z, the remain two axes will X And Y, so the
             // ParityEven is X, and the ParityOdd is Y
             if (_system.GetFrontVector(sign) == FbxAxisSystem::eParityEven) {
-                at = math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
+                at = simd_math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
             } else {
-                at = math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
+                at = simd_math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
             }
             break;
         }
@@ -205,9 +201,9 @@ vox::simd_math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem& _system) {
     // left-handed just as the enum values.
     vox::simd_math::SimdFloat4 right;
     if (_system.GetCoorSystem() == FbxAxisSystem::eRightHanded) {
-        right = math::Cross3(up, at);
+        right = simd_math::Cross3(up, at);
     } else {
-        right = math::Cross3(at, up);
+        right = simd_math::Cross3(at, up);
     }
 
     const vox::simd_math::Float4x4 matrix = {{right, up, at, vox::simd_math::simd_float4::w_axis()}};
@@ -218,18 +214,18 @@ vox::simd_math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem& _system) {
 
 FbxSystemConverter::FbxSystemConverter(const FbxAxisSystem& _from_axis, const FbxSystemUnit& _from_unit) {
     // Build axis system conversion matrix.
-    const math::Float4x4 from_matrix = BuildAxisSystemMatrix(_from_axis);
+    const simd_math::Float4x4 from_matrix = BuildAxisSystemMatrix(_from_axis);
 
     // Finds unit conversion ratio to meters, where GetScaleFactor() is in cm.
     const float to_meters = static_cast<float>(_from_unit.GetScaleFactor()) * .01f;
 
     // Builds conversion matrices.
-    convert_ = Invert(from_matrix) * math::Float4x4::Scaling(math::simd_float4::Load1(to_meters));
+    convert_ = Invert(from_matrix) * simd_math::Float4x4::Scaling(simd_math::simd_float4::Load1(to_meters));
     inverse_convert_ = Invert(convert_);
     inverse_transpose_convert_ = Transpose(inverse_convert_);
 }
 
-math::Float4x4 FbxSystemConverter::ConvertMatrix(const FbxAMatrix& _m) const {
+simd_math::Float4x4 FbxSystemConverter::ConvertMatrix(const FbxAMatrix& _m) const {
     const vox::simd_math::Float4x4 m = {{
             vox::simd_math::simd_float4::Load(static_cast<float>(_m[0][0]), static_cast<float>(_m[0][1]),
                                               static_cast<float>(_m[0][2]), static_cast<float>(_m[0][3])),
@@ -244,42 +240,39 @@ math::Float4x4 FbxSystemConverter::ConvertMatrix(const FbxAMatrix& _m) const {
 }
 
 Vector3F FbxSystemConverter::ConvertPoint(const FbxVector4& _p) const {
-    const math::SimdFloat4 p_in = math::simd_float4::Load(static_cast<float>(_p[0]), static_cast<float>(_p[1]),
-                                                          static_cast<float>(_p[2]), 1.f);
-    const math::SimdFloat4 p_out = convert_ * p_in;
+    const simd_math::SimdFloat4 p_in = simd_math::simd_float4::Load(
+            static_cast<float>(_p[0]), static_cast<float>(_p[1]), static_cast<float>(_p[2]), 1.f);
+    const simd_math::SimdFloat4 p_out = convert_ * p_in;
     Vector3F ret;
-    math::Store3PtrU(p_out, &ret.x);
+    simd_math::Store3PtrU(p_out, &ret.x);
     return ret;
 }
 
 Vector3F FbxSystemConverter::ConvertVector(const FbxVector4& _p) const {
-    const math::SimdFloat4 p_in = math::simd_float4::Load(static_cast<float>(_p[0]), static_cast<float>(_p[1]),
-                                                          static_cast<float>(_p[2]), 0.f);
-    const math::SimdFloat4 p_out = inverse_transpose_convert_ * p_in;
+    const simd_math::SimdFloat4 p_in = simd_math::simd_float4::Load(
+            static_cast<float>(_p[0]), static_cast<float>(_p[1]), static_cast<float>(_p[2]), 0.f);
+    const simd_math::SimdFloat4 p_out = inverse_transpose_convert_ * p_in;
     Vector3F ret;
-    math::Store3PtrU(p_out, &ret.x);
+    simd_math::Store3PtrU(p_out, &ret.x);
     return ret;
 }
 
-bool FbxSystemConverter::ConvertTransform(const FbxAMatrix& _m, math::Transform* _transform) const {
+bool FbxSystemConverter::ConvertTransform(const FbxAMatrix& _m, ScalableTransform* _transform) const {
     assert(_transform);
 
-    const math::Float4x4 matrix = ConvertMatrix(_m);
+    const simd_math::Float4x4 matrix = ConvertMatrix(_m);
 
-    math::SimdFloat4 translation, rotation, scale;
+    simd_math::SimdFloat4 translation, rotation, scale;
     if (ToAffine(matrix, &translation, &rotation, &scale)) {
-        vox::simd_math::Transform transform;
-        math::Store3PtrU(translation, &_transform->translation.x);
-        math::StorePtrU(math::Normalize4(rotation), &_transform->rotation.x);
-        math::Store3PtrU(scale, &_transform->scale.x);
+        vox::ScalableTransform transform;
+        simd_math::Store3PtrU(translation, &_transform->translation.x);
+        simd_math::StorePtrU(simd_math::Normalize4(rotation), &_transform->rotation.x);
+        simd_math::Store3PtrU(scale, &_transform->scale.x);
         return true;
     }
 
     // Failed to decompose matrix, reset transform to identity.
-    *_transform = vox::simd_math::Transform::identity();
+    *_transform = vox::ScalableTransform::identity();
     return false;
 }
-}  // namespace fbx
-}  // namespace offline
-}  // namespace animation
-}  // namespace vox
+}  // namespace vox::animation::offline::fbx
--- converter/fbx.h
+++ converter/fbx.h
@@ -8,15 +8,13 @@
 
 #include <fbxsdk.h>
 
-#include "vox.animation/offline/fbx/export.h"
+#include "converter/export.h"
 #include "vox.math/scalable_transform.h"
 #include "vox.simd_math/simd_math.h"
 
 namespace vox {
 struct ScalableTransform;
-namespace animation {
-namespace offline {
-namespace fbx {
+namespace animation::offline::fbx {
 
 // Manages FbxManager instance.
 class VOX_ANIMFBX_DLL FbxManagerInstance {
@@ -44,7 +42,7 @@ public:
     virtual ~FbxDefaultIOSettings();
 
     // Get FbxIOSettings instance.
-    FbxIOSettings* settings() const { return io_settings_; }
+    [[nodiscard]] FbxIOSettings* settings() const { return io_settings_; }
 
     // Cast operator to get internal FbxIOSettings instance.
     operator FbxIOSettings*() const { return io_settings_; }
@@ -53,13 +51,13 @@ private:
     FbxIOSettings* io_settings_;
 };
 
-// Io settings used to import an animation from a scene.
+// Io's settings used to import an animation from a scene.
 class VOX_ANIMFBX_DLL FbxAnimationIOSettings : public FbxDefaultIOSettings {
 public:
     FbxAnimationIOSettings(const FbxManagerInstance& _manager);
 };
 
-// Io settings used to import a skeleton from a scene.
+// Io's settings used to import a skeleton from a scene.
 class VOX_ANIMFBX_DLL FbxSkeletonIOSettings : public FbxDefaultIOSettings {
 public:
     FbxSkeletonIOSettings(const FbxManagerInstance& _manager);
@@ -76,35 +74,35 @@ public:
     // Initialize converter with fbx scene systems.
     FbxSystemConverter(const FbxAxisSystem& _from_axis, const FbxSystemUnit& _from_unit);
 
-    // Converts a fbx matrix to an vox Float4x4 matrix, in vox axis and unit
+    // Converts a fbx matrix to a vox Float4x4 matrix, in vox axis and unit
     // systems, using _m' = C * _m * (C-1) operation.
-    math::Float4x4 ConvertMatrix(const FbxAMatrix& _m) const;
+    [[nodiscard]] simd_math::Float4x4 ConvertMatrix(const FbxAMatrix& _m) const;
 
-    // Converts fbx matrix to an vox transform, in vox axis and unit systems,
+    // Converts fbx matrix to a vox transform, in vox axis and unit systems,
     // using _m' = C * _m * (C-1) operation.
     // Can return false if matrix isn't affine.
-    bool ConvertTransform(const FbxAMatrix& _m, math::Transform* _transform) const;
+    bool ConvertTransform(const FbxAMatrix& _m, ScalableTransform* _transform) const;
 
-    // Converts fbx FbxVector4 point to an vox Float3, in vox axis and unit
+    // Converts fbx FbxVector4 point to a vox Float3, in vox axis and unit
     // systems, using _p' = C * _p operation.
-    Vector3F ConvertPoint(const FbxVector4& _p) const;
+    [[nodiscard]] Vector3F ConvertPoint(const FbxVector4& _p) const;
 
-    // Converts fbx FbxVector4 vector to an vox Float3, in vox axis and unit
+    // Converts fbx FbxVector4 vector to a vox Float3, in vox axis and unit
     // systems, using _p' = ((C-1)-T) * _p operation. Normals are converted
     // using the inverse transpose matrix to support non-uniform scale
     // transformations.
-    Vector3F ConvertVector(const FbxVector4& _p) const;
+    [[nodiscard]] Vector3F ConvertVector(const FbxVector4& _p) const;
 
 private:
     // The matrix used to convert from "from" axis/unit to vox coordinate system
     // base.
-    math::Float4x4 convert_;
+    simd_math::Float4x4 convert_;
 
     // The inverse of convert_ matrix.
-    math::Float4x4 inverse_convert_;
+    simd_math::Float4x4 inverse_convert_;
 
     // The transpose inverse of convert_ matrix.
-    math::Float4x4 inverse_transpose_convert_;
+    simd_math::Float4x4 inverse_transpose_convert_;
 };
 
 // Loads a scene from a Fbx file.
@@ -124,14 +122,14 @@ public:
     ~FbxSceneLoader();
 
     // Returns a valid scene if fbx import was successful, nullptr otherwise.
-    FbxScene* scene() const { return scene_; }
+    [[nodiscard]] FbxScene* scene() const { return scene_; }
 
     // Returns a valid converter if fbx import was successful, nullptr otherwise.
     FbxSystemConverter* converter() { return converter_; }
 
 private:
     void ImportScene(FbxImporter* _importer,
-                     const bool _initialized,
+                     bool _initialized,
                      const char* _password,
                      const FbxManagerInstance& _manager,
                      const FbxDefaultIOSettings& _io_settings);
@@ -142,7 +140,5 @@ private:
     // Axis and unit conversion helper.
     FbxSystemConverter* converter_;
 };
-}  // namespace fbx
-}  // namespace offline
 }  // namespace animation
 }  // namespace vox
